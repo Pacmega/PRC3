2bd7f92134c5ea2de9f8e7fd36d3fc8265d4f14e
Teeffelen,Joran J.J. van <joran.vanteeffelen@student.fontys.nl>
2017-08-30 14:31:15 +0200

    Car.cpp and Limousine.cpp modified

--- CarRental_cpp/src/Car.cpp
+++ CarRental_cpp/src/Car.cpp
@@ -1,6 +1,63 @@
 #include <stdexcept>
+#include <cstddef>
+#include <string>
 using namespace std;
 
 #include "Car.h"
 
 // TODO: implement your class methods here
+
+Car::Car(std::string manufacturer, std::string model, int buildYear, std::string licencePlate, double pricePerKm)
+{
+	if ((manufacturer == NULL) || (model == NULL) || (licencePlate == NULL))
+	{
+		throw std::invalid_argument("A constructorvalue is NULL")
+	}
+	Manufacturer = manufacturer;
+	Model = model;
+    BuildYear = buildYear;
+    LicencePlate = licencePlate;
+    Kilometers = 0;
+    IsAvailable = true;
+    this.pricePerKm = pricePerKm;
+}
+
+Car::Rent()
+{
+    if (!IsAvailable)
+    {
+        return false;
+    }
+
+    IsAvailable = false;
+    return true;
+}
+
+Car::Return(int kilometers)
+{
+    if (IsAvailable)
+    {
+        throw new InvalidOperationException("car was not rented");
+    }
+    if (kilometers < Kilometers)
+    {
+        throw new ArgumentException("car is returned with less kilometers than it had", "kilometers");
+    }
+
+    Decimal cost = pricePerKm * (kilometers - Kilometers);
+    Kilometers = kilometers;
+    IsAvailable = true;
+    return cost;
+}
+
+Car::Clean()
+{
+
+}
+
+Car::ToString()
+{
+	return Manufacturer +
+        " - " + Model +
+        ", " + LicencePlate;
+}
--- CarRental_cpp/src/Limousine.cpp
+++ CarRental_cpp/src/Limousine.cpp
@@ -1,3 +1,21 @@
 #include "Limousine.h"
 
 // TODO: implement your class methods here
+
+Limousine::Limousine(std::string manufacturer, std::string model, int buildYear, std::string licencePlate, bool hasMinibar) : Car( manufacturer, model, buildYear, licencePlate, pricePerKm)
+{
+	HasMinibar = hasMinibar;
+	needsCleaning = False;
+}
+
+Limousine::Return(int kilometers)
+{
+	double price = Car::Return(kilometers);
+	needsCleaning = true;
+	return price;
+}
+
+Limousine::Clean()
+{
+	needsCleaning = False;
+}
