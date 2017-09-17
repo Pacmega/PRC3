aea65c00569e89e981a0ac8cea8a1a4c71806bfc
Teeffelen,Joran J.J. van <joran.vanteeffelen@student.fontys.nl>
2017-09-17 12:20:08 +0200

    Added new assignment

---
+++ PRC3_Memory_Management_2/product/Wheel.h
@@ -0,0 +1,40 @@
+#ifndef WHEEL_H_
+#define WHEEL_H_
+
+#include <string>
+
+/*
+ *  Tip: destructor, copy constructor en assignment operator overloading is not
+ *       needed as this class does not contain allocated memory
+*/
+
+class Wheel
+{
+  private:
+    int diameter;
+    std::string material;
+
+  public:
+    Wheel(int diameter, const std::string& material);
+    /* pre : d > 0
+       post: A Wheel object is created with the given material and diameter
+    */
+
+    const std::string& getMaterial();
+    /* pre : -
+       post: getMaterial()= <Wheel>'s material
+    */
+
+    void setMaterial(const std::string& material);
+    /* pre : -
+       post: <Wheel>'s material is now material
+    */
+
+    int getDiameter();
+    /* pre : -
+       post getDiameter() = <Wheel>'s diameter
+    */
+
+};
+
+#endif
