f8be8dbc7014edccc00e910a15df201ce118dfc8
Bas <bas.witters@hotmail.com>
2017-09-20 10:50:33 +0200

    Coded indexInRange

--- PRC3_Memory_Management_2/product/Car.cpp
+++ PRC3_Memory_Management_2/product/Car.cpp
@@ -10,8 +10,28 @@ Car::Car(const std::string& model, const std::string& material, int diameter, in
 
 Car::~Car()
 {
-	
+
 }
+
+// Private functions
+
+bool Car::indexInRange(int index)
+{
+	int actualWheels = wheels.size();
+
+	// In the header file it is defined as a pre that a car has at least 2 wheels.
+	if (index > 2 && index < actualWheels)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+// Public functions
 
 const std::string& Car::getModel()
 {
