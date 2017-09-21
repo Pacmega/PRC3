00b9efe3961fdab0536a81d4e483e5d849b08283
4maself <joranvt@gmail.com>
2017-09-21 19:10:09 +0200

    Added a little test to try out the Gtest environment

--- PRC3_Memory_Management_2/test/CarTest.cpp
+++ PRC3_Memory_Management_2/test/CarTest.cpp
@@ -1,5 +1,13 @@
+#include <gtest/gtest.h>
+
+TEST(TestingTest, TestedTest)
+{
+	// This test is just a test to test the GoogleTest testing environment
+	EXPECT_TRUE( add(2,2) == 4 );
+}
+
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
