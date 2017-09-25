af919b9eaa833582c77459e4d59b3c54ae013487
Teeffelen,Joran J.J. van <joran.vanteeffelen@student.fontys.nl>
2017-09-25 21:06:21 +0200

    Edited the tests to atleast be functioning

--- PRC3_Memory_Management_2/test/CarTest.cpp
+++ PRC3_Memory_Management_2/test/CarTest.cpp
@@ -1,37 +1,34 @@
 #include <gtest/gtest.h>
 #include "Car.h"
-/*
+
 TEST(TestingTest, TestedTest) // TestingTest is a test_case_name and TestedTest is a test_name
 {
 	// This test is just a test to test the GoogleTest testing environment
-	EXPECT_TRUE(2 + 2 == 4);
+	EXPECT_TRUE( (2+2) == 4 );
 }
 
 TEST(Construct, TestRealisticInput)
 {
-	Car TestCar = new Car("Henk 500", "Hout", 2, 4);
-
-	TEST_
+	EXPECT_NO_THROW(new Car("Henk 500", "Hout", 2, 4););
 }
 
 TEST(Construct, TestZeroDiameter)
 {
-	Car TestCar = new Car("Henk 500", "Hout", 0, 4);
-
-	EXPECT_THROW(diameter, out_of_range);
+	int diameter = 0;
+	int nrWheels = 4;
+	EXPECT_THROW(new Car("Henk 500", "Hout", diameter, nrWheels), std::out_of_range);
 }
 
 TEST(Construct, TestWheelsLessThanTwo)
 {
-	Car TestCar = new Car("Henk 500", "Hout", 2, 1);
-
-	EXPECT_THROW(nrWheels, out_of_range);
+	int diameter = 2;
+	int nrWheels = 1;
+	EXPECT_THROW(new Car("Henk 500", "Hout", diameter, nrWheels), std::out_of_range);
 }
-*/
+
+
 int main(int argc, char *argv[])
 {
-	testing::InitGoogleTest(&argc, argv);
-	
-	return 0;
-	//return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
