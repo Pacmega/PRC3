da1f96fe793154c5f34a8e870ab35d2dce91a6fd
Teeffelen,Joran J.J. van <joran.vanteeffelen@student.fontys.nl>
2017-09-26 22:26:59 +0200

    All dem Tests finished

--- PRC3_Memory_Management_2/test/TruckTest.cpp
+++ PRC3_Memory_Management_2/test/TruckTest.cpp
@@ -5,8 +5,21 @@
 TEST(Construct, CreateTruckCorrect)
 {
 	std::string model = "Burger on Wheels";
-	
+	std::string material = "Rubber";
+	int diameter = 15;
+	int nrWheels = 6;
+	int power = 9000;
+
+	Truck * TestTruck = new Truck(model, material, diameter, nrWheels, power);
+
+	EXPECT_TRUE(TestTruck->getModel() == model);
+	EXPECT_TRUE(TestTruck->getNrWheels()== nrWheels);	
+	EXPECT_TRUE(TestTruck->getPower() == power);
+
+	delete TestTruck;
+
 }
+
 
 int main(int argc, char *argv[])
 {
