69cdabf952a41335f6c4a068552029959ce99acd
Pacmega <bas.witters@hotmail.com>
2017-11-26 06:31:56 -0800

    Cleaned up code

--- sortStartPoint4Students/code/testv2.cpp
+++ sortStartPoint4Students/code/testv2.cpp
@@ -62,3 +62,28 @@ public:
         return elements;
     }
 };
+
+void populateList(List &list)
+{
+    list.additem((char*)"zaterdag");
+    list.additem((char*)"zondag");
+    list.additem((char*)"maandag");
+    list.additem((char*)"dinsdag");
+    list.additem((char*)"woensdag");
+    list.additem((char*)"donderdag");
+    list.additem((char*)"vrijdag");
+    list.additem((char*)"school");
+    list.additem((char*)"laptop");
+    list.additem((char*)"pc master race");
+}
+
+int main(void)
+{
+    List* list = new List();
+    populateList(*list);
+
+    // Debugging: check how many items the list contains
+    // std::cout << "Number of elements in list: " << list->nrOfElements() << std::endl;
+
+    //sort(list->head, 0, list->nrOfElements());//first item, left index, right index(nrOfElements)
+}
