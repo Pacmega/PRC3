d5215fcb78958e82a7654f2c497e6d7b49176dfb
4maself <joranvt@gmail.com>
2017-12-01 11:49:38 +0100

    Trying to implement ** for the &head

--- sortStartPoint4Students/code/main.cpp
+++ sortStartPoint4Students/code/main.cpp
@@ -2,52 +2,54 @@
 
 #include "FileStructure.h"
 #include "Key.h"
+/* Sources:
+https://www.youtube.com/watch?v=jOyYaBHyN28
+https://stackoverflow.com/questions/5630994/merge-sorting-a-linked-list
+*/
 
-//https://stackoverflow.com/questions/5630994/merge-sorting-a-linked-list
+typedef struct Key * KeyPtr;
 
-Key* mergeSort(Key* keyToSort);
-Key* Merge(Key* firstKey, Key* secondKey);
-Key* Split(Key* keyToSplit);
+KeyPtr mergeSort(KeyPtr keyToSort);
+KeyPtr Merge(KeyPtr firstKey, KeyPtr secondKey);
+KeyPtr Split(KeyPtr keyToSplit);
 
 int main()
 {
     FileStructure f;
-    Key head;
+    KeyPtr head;
     
-    f.loadFile("data/gibberish.bin", head);
+    f.loadFile("data/gibberish.bin", *head);
 
     // next line is only to show what kind of data we're working with
     // remove this line to increase performance!
 
     /*
-    head.addValue("Amandelen");
-    head.addValue("Pannenkoek");
-    head.addValue("Boter");
-    head.addValue("Kaas");
-    head.addValue("Eieren");
-    head.addValue("Aardbei");
-    head.addValue("Handelen");
-    head.addValue("Aardappel");
+    head->addValue("Amandelen");
+    head->addValue("Pannenkoek");
+    head->addValue("Boter");
+    head->addValue("Kaas");
+    head->addValue("Eieren");
+    head->addValue("Aardbei");
+    head->addValue("Handelen");
+    head->addValue("Aardappel");
     */
-
-    std::cout << "After mergeSort" << std::endl;
     
     mergeSort(&head);
 
-    head.print();
+    head->print();
     // sort all data
     // todo: call your sort method(s) here!
 
     // save sorted data into a new file called sorted.bin
-    f.saveFile(head, "sorted.bin");
+    f.saveFile(*head, "sorted.bin");
     
     return 0;
 }
 
 // This method releases the nextValue of the key from the linked list:  Current->Next(This one goes out)->After
-Key* Split(Key* keyToSplit)
+KeyPtr Split(KeyPtr keyToSplit)
 {
-    Key* tmpKey;
+    KeyPtr tmpKey;
 
     if (keyToSplit == NULL)                                             // If the key is NULL, there is nothing to be done
         return NULL;
@@ -64,7 +66,7 @@ Key* Split(Key* keyToSplit)
 }
 
 // This method checks if the keys are in the correct spot and moves them depending on the alphabetical order
-Key* Merge(Key* firstKey, Key* secondKey)
+KeyPtr Merge(KeyPtr firstKey, KeyPtr secondKey)
 {
     if (firstKey == NULL)
         return secondKey;
@@ -77,23 +79,23 @@ Key* Merge(Key* firstKey, Key* secondKey)
     }
     else
     {
+        // secondKey->setPrev(firstKey->getPrev());
         secondKey->setNext(Merge(firstKey, secondKey->getNext()));      // If the secondKey should be in front of the firstKey, it will move the other keys aswell
         return secondKey;                                               // Finally, return the secondKey
     }
 }
 
 // This method uses both methods above to sort a linked list of keys in alphabetical order.
-Key* mergeSort(Key* keyToSort)
+KeyPtr mergeSort(KeyPtr *keyToSort)
 {
-    Key* tmpKey;
+    KeyPtr head = *keyToSort;
+    KeyPtr tmpKey;
 
-    if (keyToSort == NULL)                                              // If the key is NULL, do nothing.
-        return NULL;
-    else if (keyToSort->getNext() == NULL)                              // If it has no other keys, do nothing.
-        return keyToSort;
+    if (head == NULL || head->getNext() == NULL)                         // If the key or the next key is NULL, there is nothing to sort.
+        return head; 
     else
     {
-        tmpKey = Split(keyToSort);                                      // Split the keys into smaller groups
-        return Merge(mergeSort(keyToSort), mergeSort(tmpKey));          // Merge those keys again
+        tmpKey = Split(head);                                      // Split the keys into smaller groups
+        return Merge(mergeSort(head), mergeSort(tmpKey));                 // Merge those keys again
     }
 }
